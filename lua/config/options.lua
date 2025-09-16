6ac511152ccc3551eef09319dece548808ca8089
Donghyun Koh <andy1902@snu.ac.kr>
2025-09-16 07:40:14 +0000

    Add wrapping option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Set wrap true
+-- vim.opt.wrap = true
 
 -- LSP Server to use for Rust.
 -- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
